5d5743d095e871fd4220200fbd56bea021470a11
Clmt2707 <clement.deboeuf@gmail.com>
2024-05-21 15:31:51 +0200

    ajout effet apparition navbar/popup/img

---
+++ index.js
@@ -0,0 +1,33 @@
+let playOnce = true;
+
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.style.height = '45px';
+    } else {
+        navbar.style.height = '60px'
+    }
+
+
+    //Image
+    let scrollValue = 
+        (window.scrollY + window.innerHeight) / document.body.offsetHeight;
+        console.log(scrollValue);
+    if (scrollValue > 0.45) {
+        imgImprovise.style.opacity = 1;
+        imgImprovise.style.transform = 'none';
+    }
+
+    //Pop-up
+    if (scrollValue > 0.85 && playOnce) {
+        popup.style.opacity = 1;
+        popup.style.transform = 'none';
+        playOnce = false;
+    }
+});
+
+closeBtn.addEventListener('click', () => {
+    popup.style.opacity = 0;
+    popup.style.transform = 'translateX(500px)';
+});
